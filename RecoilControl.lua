3e3ab55f6fce3827e520ea3f81b7f297a3434edb
mazt / flaws <110755184+iosflaws@users.noreply.github.com>
2023-12-23 11:27:16 -0700

    Create RecoilControl.lua
    Recoil Control lua for Logitech G Hub and Logitech Devices

---
+++ RecoilControl.lua
@@ -0,0 +1,56 @@
+local RecoilControlStrength = 7 -- Value MUST be ROUNDED! No Decimal values such as 6.5!
+local ForceToggle = true -- Change to false if you want it to always be on.
+local ToggleKey = "CapsLock" -- Usable Keys: "CapsLock", "NumLock", "ScrollLock"
+
+
+--   HOW TO USE FOR DUMBIES, 
+--   1: Adjust your Recoil Control Strength, personally I use a value between 5-7
+--   2: Toggle it on (if you have ForceToggle enabled, else just do step 4.)
+--   3: Hold Mouse1 and Mouse2 (LeftClick and RightClick) and it should drag your cursor downwards.
+--   4: Aim at your target and your done!
+
+-- Things to note:
+--    1: I did NOT make the Recoil Control Code, I just simplified everything, so you can adjust it to your needs
+--    2: You don't need to hold down your toggle key, its a toggle.
+--    3: You must hold mouse1 and mouse2, or else IT WONT WORK!
+
+
+
+
+
+
+
+
+
+
+OutputLogMessage("@mjzt on Youtube!\n")
+EnablePrimaryMouseButtonEvents  (true);
+function OnEvent(event,arg)
+if ForceToggle ~= false then
+    if IsKeyLockOn("CapsLock")then
+        if IsMouseButtonPressed(3)then
+            repeat
+                if IsMouseButtonPressed(1) then
+                    repeat
+                        MoveMouseRelative(0,RecoilControlStrength)
+                        Sleep(7)
+                    until not IsMouseButtonPressed(1)
+                end
+            until not IsMouseButtonPressed(3)
+        end
+    end
+    
+else 
+
+        if IsMouseButtonPressed(3)then
+            repeat
+                if IsMouseButtonPressed(1) then
+                    repeat
+                        MoveMouseRelative(0,RecoilControlStrength)
+                        Sleep(7)
+                    until not IsMouseButtonPressed(1)
+                end
+            until not IsMouseButtonPressed(3)
+        end
+    end
+end
