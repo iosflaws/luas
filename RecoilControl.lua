26da4b840f5259a23954a416def455d6af837182
mazt / flaws <110755184+iosflaws@users.noreply.github.com>
2023-12-23 17:12:12 -0700

    Update RecoilControl.lua

--- RecoilControl.lua
+++ RecoilControl.lua
@@ -20,18 +20,38 @@
 ⠀⠘⣾⡀⡏⠙⡗⠻⠟⢻⠚⢻⢹⠙⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠈⠓⠧⠴⣇⣴⣄⢼⣤⠟⠚⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
-                        ANTI-RECOIL SCRIPT V2.0 by @mjzt on YOUTUBE
+                        RECOIL CONTROL SCRIPT (RCS) V1.6 by @mjzt on YOUTUBE
 
 
+
+
+
+Patch Notes for Version 1.6:
+    Added Preset Recoil control modes,
+    Added Adjustable DelayRate,
+
+Possible Future Features:
+  Bloom Reduction (Will just attempt to patch bloom position, by reseting weapon fire.)
+  Require ADS (If set to false, it'll continue, without the need of ADS'ing
 --]]
 
-local RecoilControlStrength = 7 -- Value MUST be ROUNDED! No Decimal values such as 6.5!
-local ForceToggle = true -- Change to false if you want it to always be on.
-local ToggleKey = "CapsLock" -- Usable Keys: "CapsLock", "NumLock", "ScrollLock"
+
+RecoilControlMode = "Custom" --- Presets: "Low", "Medium", "High", "Ultra", "Insanity", "Custom"
+RcCustomStrength = 7 -- Value MUST be ROUNDED! No Decimal values such as 6.5!
+RequireToggle = true -- Change to false if you want it to always be on.
+ToggleKey = "CapsLock" -- Usable Keys: "CapsLock", "NumLock", "ScrollLock"
+DelayRate = 7 -- DO NOT CHANGE IF YOU DON'T KNOW WHAT YOU'RE DOING.
+
+
+
+
+
+
 
 --[[
        HOW TO USE FOR DUMBIES, 
-1: Adjust your Recoil Control Strength, personally I use a value between 5-7
+       
+1: Adjust your Recoil Control Strength, either by using a preset or a custom value!
 2: Toggle it on (if you have ForceToggle enabled, else just do step 4.)
 3: Hold Mouse1 and Mouse2 (LeftClick and RightClick) and it should drag your cursor downwards.
 4: Aim at your target and your done!
@@ -40,6 +60,7 @@ Things to note:
 1: I did NOT make the Recoil Control Code, I just simplified everything, so you can adjust it to your needs
 2: You don't need to hold down your toggle key, its a toggle.
 3: You must hold mouse1 and mouse2, or else IT WONT WORK!
+4: ThrottleRate Works in MS, the lower the faster. 7ms = 0.07s, 2000ms = 2s. However the preset throttle rate is perfect for most FPS games. 
 --]]
 
 
@@ -49,19 +70,33 @@ Things to note:
 
 
 
-
+-------------  Recoil Presets ------
+if RecoilControlMode == "Low" then
+  RecoilControlStrength = 2
+elseif RecoilControlMode == "Medium" then
+  RecoilControlStrength = 6
+elseif RecoilControlMode == "High" then
+  RecoilControlStrength = 8
+elseif RecoilControlMode == "Ultra" then
+  RecoilControlStrength = 12
+elseif RecoilControlMode == "Insanity" then
+  RecoilControlStrength = 32
+elseif RecoilControlMode == "Custom" then
+  RecoilControlStrength = RcCustomStrength
+end
+--------------------------------------
 
 OutputLogMessage("@mjzt on Youtube!\n")
 EnablePrimaryMouseButtonEvents  (true);
 function OnEvent(event,arg)
-if ForceToggle ~= false then
-    if IsKeyLockOn("CapsLock")then
+if RequireToggle ~= false then
+    if IsKeyLockOn(ToggleKey)then
         if IsMouseButtonPressed(3)then
             repeat
                 if IsMouseButtonPressed(1) then
                     repeat
                         MoveMouseRelative(0,RecoilControlStrength)
-                        Sleep(7)
+                        Sleep(DelayRate)
                     until not IsMouseButtonPressed(1)
                 end
             until not IsMouseButtonPressed(3)
@@ -75,7 +110,7 @@ else
                 if IsMouseButtonPressed(1) then
                     repeat
                         MoveMouseRelative(0,RecoilControlStrength)
-                        Sleep(7)
+                        Sleep(DelayRate)
                     until not IsMouseButtonPressed(1)
                 end
             until not IsMouseButtonPressed(3)
