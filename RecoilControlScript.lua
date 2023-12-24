15f330a73a97ee3e7332db98b28dfa01353f0b14
mazt / flaws <110755184+iosflaws@users.noreply.github.com>
2023-12-23 22:08:10 -0700

    Update RecoilControlScript.lua

--- RecoilControlScript.lua
+++ RecoilControlScript.lua
@@ -22,7 +22,7 @@ ToggleBind = "4" -- Works as Mouse buttons, these have to be held.
 ⠀⠘⣾⡀⡏⠙⡗⠻⠟⢻⠚⢻⢹⠙⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠈⠓⠧⠴⣇⣴⣄⢼⣤⠟⠚⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
-                        RECOIL CONTROL SCRIPT (RCS) V1.7 by @mjzt on YOUTUBE
+                                    R E C O I L  C O N T R O L  S C R I P T  (R C S)  V 1 . 7  b y  @ m j z t  o n  Y O U T U B E
 
 
 
@@ -33,10 +33,10 @@ Patch Notes for Version 1.7:
 
 Possible Future Features:
   Bloom Reduction (Will just attempt to patch bloom position, by reseting weapon fire.)
-  Require ADS (If set to false, it'll continue, without the need of ADS'ing
+  Require ADS (If set to false, it'll continue, without the need of ADS'ing)
 --]]
 
----                                R A P I D  F I R E                                ----
+---                                 R A P I D  F I R E                                ----
 
 
 EnableRapidFire = false -- When Set to false, it'll turn off Rapid Fire!
@@ -58,13 +58,20 @@ DelayRate = 3 -- DO NOT CHANGE IF YOU DON'T KNOW WHAT YOU'RE DOING.
 
 --[[
        HOW TO USE FOR DUMBIES, 
-       
+  (RCS)
 1: Adjust your Recoil Control Strength, either by using a preset or a custom value!
 2: Toggle it on (if you have ForceToggle enabled, else just do step 4.)
 3: Hold Mouse1 and Mouse2 (LeftClick and RightClick) and it should drag your cursor downwards.
 4: Aim at your target and your done!
 
-Things to note:
+(RAPID FIRE)
+1: Turn it on.
+2: Hold the MOUSE button you set it to.
+3: Use a gun that has a slow firing rate, such as cobra dmr or thermal scoped ar.
+4: Aim at your target and your done!
+
+
+(Things to note)
 1: I did NOT make the Recoil Control Code, I just simplified everything, so you can adjust it to your needs
 2: You don't need to hold down your toggle key, its a toggle.
 3: You must hold mouse1 and mouse2, or else IT WONT WORK!
@@ -128,6 +135,7 @@ else
     end
 else 
 end  
+  
 --- Rapid Fire ---
 
 if EnableRapidFire ~= false then
