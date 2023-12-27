fc3ddea261ec4409facd0e7e927299b85391706e
mazt / flaws <110755184+iosflaws@users.noreply.github.com>
2023-12-27 11:50:46 -0700

    Update RecoilControlScript.lua

--- RecoilControlScript.lua
+++ RecoilControlScript.lua
@@ -24,35 +24,36 @@ dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
 
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
-                                    R E C O I L  C O N T R O L  S C R I P T  (R C S)  V 2 . 0  b y  @ m j z t  o n  Y O U T U B E
+                                    R E C O I L  C O N T R O L  S C R I P T  (R C S)  V 2 . 1  b y  @ m j z t  o n  Y O U T U B E
 
 
 
 
 
-Patch Notes for Version 2.0:
+Patch Notes for Version 2.1:
     Removed Rapid-Fire
-    Added Bloom Reduction
+    Removed Bloom-Reduction
 
 Possible Future Features:
   Require ADS (If set to false, it'll continue, without the need of ADS'ing)
 --]]
 
----                             B L O O M  R E D U C T I O N                                ----
 
-
-EnableBloomReduction = false -- When Set to false, it'll turn off Bloom Reduction!
-BloomReductionMode = "High" --- Presets: "Low", "Medium", "High", "Ultra"
-BRBind = "4" --- Mouse Buttons, not Keyboard. MUST be HELD DOWN.
 
 
 ---                              R E C O I L  C O N T R O L                               ----
 
-EnableRCS = false --- When Set to false, it'll turn off Recoil Control!
-RecoilControlMode = "Custom" --- Presets: "Low", "Medium", "High", "Ultra", "Insanity", "Custom"
+
+EnableRCS = true --- When Set to false, it'll turn off Recoil Control!
+
+RecoilControlMode = "Medium" --- Presets: "Low", "Medium", "High", "Ultra", "Insanity", "Custom"
+
 RcCustomStrength = 7 -- Value MUST be ROUNDED! No Decimal values such as 6.5!
+
 RequireToggle = true -- Change to false if you want it to always be on.
+
 ToggleKey = "CapsLock" -- Usable Keys: "CapsLock", "NumLock", "ScrollLock"
+
 DelayRate = 7 -- DO NOT CHANGE IF YOU DON'T KNOW WHAT YOU'RE DOING.
 
 
@@ -69,11 +70,7 @@ DelayRate = 7 -- DO NOT CHANGE IF YOU DON'T KNOW WHAT YOU'RE DOING.
 4: Hold Mouse1 and Mouse2 (LeftClick and RightClick) and it should drag your cursor downwards.
 5: Aim at your target and your done!
 
-  (BR)
-1: If the "EnableBloomReduction" is set to false, change to true.
-2: Hold the MOUSE button you set it to.
-3: Use a gun that has a slow firing rate, such as cobra dmr or thermal scoped ar.
-4: Aim at your target and your done!
+
 
 
 (Things to note)
@@ -81,7 +78,6 @@ DelayRate = 7 -- DO NOT CHANGE IF YOU DON'T KNOW WHAT YOU'RE DOING.
 2: You don't need to hold down your toggle key, its a toggle.
 3: You must hold mouse1 and mouse2, or else IT WONT WORK!
 4: DelayRate Works in MS, the lower the faster. 7ms = 0.07s, 2000ms = 2s. However the preset Delay rate is perfect for most FPS games. 
-5: RAPID FIRE AND RECOIL CONTROL DON'T WORK TOGETHER CURRENTLY.
 --]]
 
 
@@ -107,17 +103,7 @@ elseif RecoilControlMode == "Custom" then
 end
 --------------------------------------
 
--------------  Bloom Reduction Presets ------
-if BloomReductionMode == "Low" then
-  BRStrength = 75
-elseif BloomReductionMode == "Medium" then
-  BRStrength = 175
-elseif BloomReductionMode == "High" then
-  BRStrength = 275
-elseif BloomReductionMode == "Ultra" then
-  BRStrength = 300
-end
---------------------------------------
+
 
 OutputLogMessage("@mjzt on Youtube!\n")
 EnablePrimaryMouseButtonEvents  (true);
@@ -152,18 +138,4 @@ else
     end
 else 
 end  
-  
---- Bloom Reduction ---
-
-if EnableBloomReduction ~= false then
-if IsMouseButtonPressed(BRBind)then
-      repeat
-        Sleep(BRStrength)
-        PressAndReleaseMouseButton(1)
-	Sleep(BRStrength)
-        until not IsMouseButtonPressed(BRBind)
-        end
-  else 
-
-      end 
 end
