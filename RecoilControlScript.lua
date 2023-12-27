9faf9c47c1a41fdd6f48c276bf26f056eda8b087
mazt / flaws <110755184+iosflaws@users.noreply.github.com>
2023-12-27 11:51:46 -0700

    Update RecoilControlScript.lua

--- RecoilControlScript.lua
+++ RecoilControlScript.lua
@@ -8,7 +8,7 @@ dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
 9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
  9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
   `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
-    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'    NO    `98v8P'  RECOIL  `XXP' `9XXXXXXXXXXXP'
         ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
                         )b.  .dbo.dP'`v'`9b.odb.  .dX(
                       ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
